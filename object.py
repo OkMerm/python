8df296a5ed3bab39d24d638be00827a3424e7204
OkMerm <145158055+OkMerm@users.noreply.github.com>
2023-12-25 00:19:29 +0300

    Практическое задание 3

---
+++ object.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*
+import numpy as np
+import numpy.typing as npt
+from typing import List
+import matplotlib.pyplot as plt
+from tools import *
+from sources import Probe, Source, AnimateFieldDisplay, showProbeSignals
+'''* импорт с получением доступа ко всем именам'''
+
+def fillMedium(layer: LayerDiscrete,
+               eps: npt.NDArray[np.float64],
+               mu: npt.NDArray[np.float64],
+               sigma: npt.NDArray[np.float64]):
+    if layer.xmax is not None:
+        eps[layer.xmin: layer.xmax] = layer.eps
+        mu[layer.xmin: layer.xmax] = layer.mu
+        sigma[layer.xmin: layer.xmax] = layer.sigma
+    else:
+        eps[layer.xmin:] = layer.eps
+        mu[layer.xmin:] = layer.mu
+        sigma[layer.xmin:] = layer.sigma
+
+if __name__ == '__main__':
+    # Используемые константы
+    # Волновое сопротивление свободного пространства
+    W0 = 120.0 * np.pi
+
+    # Скорость света в вакууме
+    c = 299792458.0
+
+    # Электрическая постоянная
+    eps0 = 8.854187817e-12
+
+    # Параметры моделирования
+    # Частота сигнала, Гц
+    f_Hz = 1.5e9
+
+    # Дискрет по пространству в м
+    dx = 4e-3
+
+    wavelength = c / f_Hz
+    Nl = wavelength / dx
+
+    # Число Куранта
+    Sc = 1.0
+
+    # Размер области моделирования в м
+    maxSize_m = 4
+    
+    # Время расчета в секундах
+    maxTime_s = 20e-9
+
+    # Положение источника в м
+    sourcePos_m = 2
+
+    # Координаты датчиков для регистрации поля в м
+    probesPos_m = [2]
+
+    # Параметры слоев
+    layers_cont = [LayerContinuous(0, eps=3.0, sigma=0.0)]
+
+    # Скорость обновления графика поля
+    speed_refresh = 15
+
+    # Переход к дискретным отсчетам
+    # Дискрет по времени
+    dt = dx * Sc / c
+
+    sampler_x = Sampler(dx)
+    sampler_t = Sampler(dt)
+
+    # Время расчета в отсчетах
+    maxTime = sampler_t.sample(maxTime_s)
+
+    # Размер области моделирования в отсчетах
+    maxSize = sampler_x.sample(maxSize_m)
+
+    # Положение источника в отсчетах
+    sourcePos = sampler_x.sample(sourcePos_m)
+
+    layers = [sampleLayer(layer, sampler_x) for layer in layers_cont]
+
+    # Датчики для регистрации поля
+    probesPos = [sampler_x.sample(pos) for pos in probesPos_m]
+    probes = [Probe(pos, maxTime) for pos in probesPos]
+
+    # Вывод параметров моделирования
+    print(f'Число Куранта: {Sc}')
+    print(f'Размер области моделирования: {maxSize_m} м')
+    print(f'Время расчета: {maxTime_s * 1e9} нс')
+    print(f'Координата источника: {sourcePos_m} м')
+    print(f'Частота сигнала: {f_Hz * 1e-9} ГГц')
+    print(f'Длина волны: {wavelength} м')
+    print(f'Количество отсчетов на длину волны (Nl): {Nl}')
+    probes_m_str = ', '.join(['{:.6f}'.format(pos) for pos in probesPos_m])
+    print(f'Дискрет по пространству: {dx} м')
+    print(f'Дискрет по времени: {dt * 1e9} нс')
+    print(f'Координаты пробника [м]: {probes_m_str}')
+    print()
+    print(f'Размер области моделирования: {maxSize} отсч.')
+    print(f'Время расчета: {maxTime} отсч.')
+    print(f'Координата источника: {sourcePos} отсч.')
+    probes_str = ', '.join(['{}'.format(pos) for pos in probesPos])
+    print(f'Координаты пробника [отсч.]: {probes_str}')
+
+    # Параметры среды
+    # Диэлектрическая проницаемость
+    eps = np.ones(maxSize)
+
+    # Магнитная проницаемость
+    mu = np.ones(maxSize - 1)
+
+    # Проводимость
+    sigma = np.zeros(maxSize)
+
+    for layer in layers:
+        fillMedium(layer, eps, mu, sigma)
+
+    # Коэффициенты для учета потерь
+    loss = sigma * dt / (2 * eps * eps0)
+    ceze = (1.0 - loss) / (1.0 + loss)
+    cezh = W0 / (eps * (1.0 + loss))
+
+    #Расчет коэффициентов для граничных условий
+    #Sc' для правой границы
+    
+    Sc1Left = Sc / np.sqrt(mu[0] * eps[0])
+
+    k1Left = -1 / (1 / Sc1Left + 2 + Sc1Left)
+    k2Left = 1 / Sc1Left - 2 + Sc1Left
+    k3Left = 2 * (Sc1Left - 1 / Sc1Left)
+    k4Left = 4 * (1 / Sc1Left + Sc1Left)
+
+    # Ez[-3: -1] в предыдущий момент времени (q)
+    oldEzLeft1 = np.zeros(3)
+
+    # Ez[-3: -1] в пред-предыдущий момент времени (q - 1)
+    oldEzLeft2 = np.zeros(3)
+
+    #Источник
+    magnitude = 1.0
+    signal = GaussianMod(1, 150, 50)
+    
+    source = Source(signal, 0.0, Sc, eps[sourcePos], mu[sourcePos])
+
+    Ez = np.zeros(maxSize)
+    Hy = np.zeros(maxSize - 1)
+
+    # Параметры отображения поля E
+    display_field = Ez
+    display_ylabel = 'Ez, В/м'
+    display_ymin = -2.1
+    display_ymax = 2.1
+
+    # Создание экземпляра класса для отображения
+    # распределения поля в пространстве
+    display = AnimateFieldDisplay(maxSize, display_ymin, display_ymax, display_ylabel, dx, dt)
+
+    display.activate()
+    display.drawSources([sourcePos])
+    display.drawProbes(probesPos)
+    for layer in layers:
+        display.drawBoundary(layer.xmin)
+        if layer.xmax is not None:
+            display.drawBoundary(layer.xmax)
+
+    for t in range(1, maxTime):
+        # Расчет компоненты поля H
+        Hy = Hy + (Ez[1:] - Ez[:-1]) * Sc / (W0 * mu)
+
+        # Источник возбуждения
+        Hy[sourcePos - 1] += source.getFieldH(t) 
+        # Ez[1] в предыдущий момент времени
+        oldEzLeft = Ez[1]
+        # Ez[-2] в предыдущий момент времени
+        oldEzRight = Ez[-2]
+        # Расчет компоненты поля E
+        Ez[1:-1] = ceze[1: -1] * Ez[1: -1] + cezh[1: -1] * (Hy[1:] - Hy[: -1])
+        # Граничные условия ABC второй степени (слева)
+        Ez[0] = (k1Left * (k2Left * (Ez[2] + oldEzLeft2[0]) +
+                             k3Left * (oldEzLeft1[0] + oldEzLeft1[2] - Ez[1] - oldEzLeft2[1]) -
+                             k4Left * oldEzLeft1[1]) - oldEzLeft2[2])
+        
+        oldEzLeft2[:] = oldEzLeft1[:]
+        oldEzLeft1[:] = Ez[0: 3]
+
+        # Источник возбуждения
+        Ez[sourcePos] += source.getFieldE(t)
+        
+        # Регистрация поля в датчиках
+        for probe in probes:
+            probe.addData(Ez, Hy)
+
+        if t % speed_refresh == 0:
+            display.updateData(display_field, t)
+            display.stop()
+
+    # Отображение сигнала, сохраненного в пробнике
+    showProbeSignals(probes, dx, dt, -2.1, 2.1)
+
+    # Построение спектра
+    plt.figure()
+    sp  = np.fft.fft(probes[0].E)
+    freq = np.fft.fftfreq(maxTime)
+    plt.plot(freq / (dt * 1e9), abs(sp) / max(abs(sp)))
+    plt.xlim(0.5, 3.5)
+    plt.grid()
+    plt.xlabel('f, ГГц')
+    display.stop()
+    plt.show()
---
+++ sources.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractmethod
+from typing import List, Optional
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+from typing import TypeVar
+
+
+class Probe:
+    '''
+    Класс для хранения временного сигнала в датчике.
+    '''
+    def __init__(self, position: int, maxTime: int):
+        '''
+        position - положение датчика (номер ячейки).
+        maxTime - максимальное количество временных
+            шагов для хранения в датчике.
+        '''
+        self.position = position
+
+        # Временные сигналы для полей E и H
+        self.E = np.zeros(maxTime)
+        self.H = np.zeros(maxTime)
+
+        # Номер временного шага для сохранения полей
+        self._time = 0
+
+    E = TypeVar("E", bound=np.generic, covariant=True)
+    H = TypeVar("H", bound=np.generic, covariant=True)
+    def addData(self, E: npt.NDArray[E], H: npt.NDArray[H]):
+        '''
+        Добавить данные по полям E и H в датчик.
+        '''
+        self.E[self._time] = E[self.position]
+        self.H[self._time] = H[self.position]
+        self._time += 1
+
+class AnimateFieldDisplay:
+    '''
+    Класс для отображения анимации распространения ЭМ волны в пространстве
+    '''
+
+    def __init__(self,
+                 maxXSize: int,
+                 minYSize: float, maxYSize: float,
+                 yLabel: str,
+                 dx: float,
+                 dt: float,
+                 title: Optional[str] = None):
+        '''
+        maxXSize - размер области моделирования в отсчетах.
+        minYSize, maxYSize - интервал отображения графика по оси Y.
+        yLabel - метка для оси Y
+        '''
+        self.maxXSize = maxXSize
+        print(self.maxXSize)
+        self.minYSize = minYSize
+        self.maxYSize = maxYSize
+        self._xList = None
+        self._line = None
+        self._xlabel = 'x, м'
+        self._ylabel = yLabel
+        self._probeStyle = 'xr'
+        self._sourceStyle = 'ok'
+        self._dx = dx
+        self._dt = dt
+        self._title = title
+
+        '''
+        dx - дискрет по пространству, м
+        dt - дискрет по времени, с (они используются позже в мэйн файле, где задаются все данные, а также ниже)
+        '''
+
+    def activate(self):
+        '''
+        Инициализировать окно с анимацией
+        '''
+        self._xList = np.arange(self.maxXSize) * self._dx
+        
+        # Включить интерактивный режим для анимации
+        plt.ion()
+
+        # Создание окна для графика
+        self._fig, self._ax = plt.subplots(figsize=(10, 6.5))
+
+        if self._title is not None:
+            self._fig.suptitle(self._title)
+
+        # Установка отображаемых интервалов по осям
+        self._ax.set_xlim(0, self.maxXSize * self._dx)
+        self._ax.set_ylim(self.minYSize, self.maxYSize)
+        
+        # Установка меток по осям
+        self._ax.set_xlabel(self._xlabel)
+        self._ax.set_ylabel(self._ylabel)
+
+        # Включить сетку на графике
+        self._ax.grid()
+
+        # Отобразить поле в начальный момент времени
+        self._line, = self._ax.plot(self._xList, np.zeros(self.maxXSize))
+
+    def drawProbes(self, probesPos: List[int]):
+        '''
+        Нарисовать датчики.
+
+        probesPos - список координат датчиков для регистрации временных
+            сигналов (в отсчетах).
+        '''
+        # Отобразить положение датчиков
+        self._ax.plot(np.array(probesPos) * self._dx,
+                      [0] * len(probesPos), self._probeStyle)
+
+        for n, pos in enumerate(probesPos):
+            self._ax.text(
+                pos * self._dx,
+                0,
+                '\n{n}'.format(n=n + 1),
+                verticalalignment='top',
+                horizontalalignment='center')
+            
+    def drawSources(self, sourcesPos: List[int]):
+        '''
+        Нарисовать источники.
+
+        sourcesPos - список координат источников (в отсчетах).
+        '''
+        # Отобразить положение источников
+        self._ax.plot(np.array(sourcesPos) * self._dx, [0] * len(sourcesPos), self._sourceStyle)
+
+    def drawBoundary(self, position: int):
+        '''
+        Нарисовать границу в области моделирования.
+
+        position - координата X границы (в отсчетах).
+        '''
+        self._ax.plot([position * self._dx, position * self._dx],
+                      [self.minYSize, self.maxYSize],
+                      '--k')
+
+    def stop(self):
+        '''
+        Остановить анимацию
+        '''
+        plt.ioff()
+
+    T = TypeVar("T", bound=np.generic, covariant=True)
+    def updateData(self, data: npt.NDArray[T], timeCount: int):
+        '''
+        Обновить данные с распределением поля в пространстве
+        '''
+        self._line.set_ydata(data)
+        self._ax.set_title(str(timeCount * self._dt * 1e9))
+        self._fig.canvas.draw()
+        self._fig.canvas.flush_events()
+
+def showProbeSignals(probes: List[Probe], dx: float, dt: float, minYSize: float, maxYSize: float):
+    
+    '''
+    Показать графики сигналов, зарегистрированых в датчиках.
+
+    probes - список экземпляров класса Probe.
+    minYSize, maxYSize - интервал отображения графика по оси Y.
+    '''
+
+    # Создание окна с графиков
+    fig, ax = plt.subplots()
+
+    # Настройка внешнего вида графиков
+    ax.set_xlim(0, len(probes[0].E) * dt * 1e9)
+    ax.set_ylim(minYSize, maxYSize)
+    ax.set_xlabel('t, нс')
+    ax.set_ylabel('Ez, В/м')
+    ax.grid()
+    
+    time_list = np.arange(len(probes[0].E)) * dt * 1e9
+
+    # Вывод сигналов в окно
+    for probe in probes:
+        ax.plot(time_list, probe.E)
+    
+    # Показать окно с графиками
+    plt.show()
+
+class Source1D(metaclass=ABCMeta):
+    '''
+    Базовый класс для всех источников одномерного метода FDTD
+    '''
+
+    @abstractmethod
+    def getFieldE(self, position, time, q,):
+        '''
+        Метод должен возвращать значение поля источника в момент времени time
+        '''
+        pass
+
+    def getFieldH(self, time):
+        return 0.0
+    
+class SourcePlaneWave(metaclass=ABCMeta):
+    @abstractmethod
+    def getFieldE(self, position, time):
+        pass
+
+class Source(Source1D):
+    def __init__(self, source: SourcePlaneWave,
+                 sourcePos: float,
+                 Sc: float = 1.0,
+                 eps: float = 1.0,
+                 mu: float = 1.0):
+        self.source = source
+        self.sourcePos = sourcePos
+        self.Sc = Sc
+        self.eps = eps
+        self.mu = mu
+        self.W0 = 120.0 * np.pi
+
+    def getFieldH(self, time):
+        return -(self.Sc / (self.W0 * self.mu)) * (
+                    self.source.getFieldE(self.sourcePos, time) - self.source.getFieldE(self.sourcePos - 1, time))
+
+    def getFieldE(self, time):
+        return (self.Sc / np.sqrt(self.eps * self.mu)) * (
+                    self.source.getFieldE(self.sourcePos - 0.5, time + 0.5) + self.source.getFieldE(self.sourcePos + 0.5,
+                                                                                          time + 0.5))
